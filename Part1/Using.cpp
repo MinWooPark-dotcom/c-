a8ddd069ffe8ea3963b208255617fd86a35a258e
Minwoo Park <positive1attraction@gmail.com>
2021-09-03 23:35:29 +0900

    using

---
+++ Part1/Using.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // Input/Output Stream: 스트림의 입출력을 위한 사항들을 선언하는 내용을 담고 있는 헤더 파일
+using namespace std;
+
+int main() // 프로그램의 시작점 main() 함수
+{
+    //  표준 출력 스트림으로 문장을 출력함
+    cout << "나의 첫 번째 C++프로그램" << endl; // cout: Character Output, endl: End Of Line
+    return 0;
+}
