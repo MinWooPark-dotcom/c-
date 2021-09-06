77e51f32c1f83cbd8c86f41c80dc035300fe15f8
Minwoo Park <positive1attraction@gmail.com>
2021-09-06 22:09:15 +0900

    inline

--- Part3/3-10.cpp
+++ Part3/3-10.cpp
@@ -1,43 +1,20 @@
-// Overloading 
 #include <iostream>
 using namespace std;
 
-struct TimeRec {
-    int hours;
-    int minutes;
-};
-
-// Function that adds time
-// Arguments 
-// - TimeRec &t1: Time to accumulate
-// - const TimeRec &ts: Time to add
-void AddTime(TimeRec &t1, const TimeRec &t2)
+inline void SwapValues(int &x, int &y)
 {
-    t1.minutes += t2.minutes;
-    t1.hours += t2.hours + t1.minutes / 60;
-    t1.minutes %= 60;
-}
-
-void AddTime(TimeRec &t, int minutes)
-{
-    t.minutes += minutes;
-    t.hours += t.minutes / 60;
-    t.minutes %= 60;
+    int temp = x;
+    x = y;
+    y = temp;
 }
 
 int main()
 {
-    TimeRec tRec1 = {2, 30};
-    TimeRec tRec2 = {1, 45};
-
-    cout << tRec1.hours << "Hours " << tRec1.minutes << "Minutes + ";
-    cout << tRec2.hours << "Hours " << tRec2.minutes << "Minutes = ";
-    AddTime(tRec1, tRec2);
-    cout << tRec1.hours << "Hours " << tRec1.minutes << "Minutes " << endl;
-
-    cout << tRec1.hours << "Hours " << tRec1.minutes << "Minutes + ";
-    cout << "135 minutes = ";
-    AddTime(tRec1, 135);
-    cout << tRec1.hours << "Hours " << tRec1.minutes << "Minutes " << endl;
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    if (a< b)
+        SwapValues(a, b);
+    cout << "A larger number = " << a << endl << "A smaller number = " << b << endl;
     return 0;
 }
