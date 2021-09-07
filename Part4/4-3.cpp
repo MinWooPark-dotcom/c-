4c7635e4d6b635ec9628add6e33ca5f9bdf5ea4c
Minwoo Park <positive1attraction@gmail.com>
2021-09-07 20:18:12 +0900

    Constructor

---
+++ Part4/4-3.cpp
@@ -0,0 +1,17 @@
+#ifndef COUNTER_H_INCLUDED
+#define COUNTER_H_INCLUDED
+
+class Counter {
+    int value;
+public:
+    Counter()  // constructor
+        { value = 0; }
+    void reset()
+    { value = 0; }
+    void count()
+    { ++value; }
+    int getValue() const
+    { return value; }
+};
+
+#endif 
