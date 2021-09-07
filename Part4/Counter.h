d148271ab6fed5fcd4b3dc8f296d284e31e1c846
Minwoo Park <positive1attraction@gmail.com>
2021-09-07 20:38:52 +0900

    Add constructor

--- Part4/Counter.h
+++ Part4/Counter.h
@@ -1,22 +1,42 @@
-#ifndef COUNTER_H_INCLUDED
+// #ifndef COUNTER_H_INCLUDED
 
+// #define COUNTER_H_INCLUDED
+
+// class Counter {
+//     int value;
+// public:
+//     void reset()
+//         {
+//             value = 0;
+//         }
+//     void count()
+//         {
+//             ++value;
+//         }
+//     int getValue() const
+//     {
+//         return value;
+//     }
+// };
+
+// #endif 
+
+// After adding the contsructor
+
+#ifndef COUNTER_H_INCLUDED
 #define COUNTER_H_INCLUDED
 
 class Counter {
     int value;
 public:
+    Counter()  // constructor
+        { value = 0; }
     void reset()
-        {
-            value = 0;
-        }
+    { value = 0; }
     void count()
-        {
-            ++value;
-        }
+    { ++value; }
     int getValue() const
-    {
-        return value;
-    }
+    { return value; }
 };
 
 #endif 
