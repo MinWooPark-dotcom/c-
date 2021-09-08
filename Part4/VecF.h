4a60fe74b0d608d7be7c8c9669895953be992684
Minwoo Park <positive1attraction@gmail.com>
2021-09-08 18:49:48 +0900

    Copy constructor

--- Part4/VecF.h
+++ Part4/VecF.h
@@ -1,3 +1,38 @@
+// 4-11
+// #ifndef VEC_F_H_INCLUDED
+// #define VEC_F_H_INCLUDED
+// #include <iostream>
+// #include <cstring>
+// using namespace std;
+
+// class VecF  {
+//     int n;
+//     float *arr;
+// public:
+//     VecF(int d, float* a = nullptr): n (d) {
+//         arr = new float[d];
+//         if(a)
+//             memcpy(arr, a, sizeof(float) * n);
+//     }
+//     ~VecF() {
+//         delete[] arr;
+//     }
+//     VecF add(const VecF& fv) const {
+//         VecF tmp(n);
+//         for (int i = 0; i < n; i++)
+//             tmp.arr[i] = arr[i] + fv.arr[i];
+//         return tmp;
+//     }
+//     void print() const {
+//         cout << "[";
+//         for (int i = 0; i < n; i++)
+//             cout << arr[i] << "";
+//         cout << "]";
+//     }
+// };
+// #endif 
+
+// 4-13
 #ifndef VEC_F_H_INCLUDED
 #define VEC_F_H_INCLUDED
 #include <iostream>
@@ -16,6 +51,11 @@ public:
     ~VecF() {
         delete[] arr;
     }
+    VecF(const VecF& fv): n (fv.n) {
+        arr = new float[n];
+        memcpy(arr, fv.arr, sizeof(float) * n);
+    }
+
     VecF add(const VecF& fv) const {
         VecF tmp(n);
         for (int i = 0; i < n; i++)
