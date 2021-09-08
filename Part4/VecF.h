21e667797662f2e0c7637f1b5bd47909c951c851
Minwoo Park <positive1attraction@gmail.com>
2021-09-08 19:57:15 +0900

    Move constructor

--- Part4/VecF.h
+++ Part4/VecF.h
@@ -32,7 +32,47 @@
 // };
 // #endif 
 
-// 4-13
+// // 4-13
+// #ifndef VEC_F_H_INCLUDED
+// #define VEC_F_H_INCLUDED
+// #include <iostream>
+// #include <cstring>
+// using namespace std;
+
+// class VecF  {
+//     int n;
+//     float *arr;
+// public:
+//     VecF(int d, float* a = nullptr): n (d) {
+//         arr = new float[d];
+//         if(a)
+//             memcpy(arr, a, sizeof(float) * n);
+//     }
+//     ~VecF() {
+//         delete[] arr;
+//     }
+//     VecF(const VecF& fv): n (fv.n) {
+//         arr = new float[n];
+//         memcpy(arr, fv.arr, sizeof(float) * n);
+//     }
+
+//     VecF add(const VecF& fv) const {
+//         VecF tmp(n);
+//         for (int i = 0; i < n; i++)
+//             tmp.arr[i] = arr[i] + fv.arr[i];
+//         return tmp;
+//     }
+//     void print() const {
+//         cout << "[";
+//         for (int i = 0; i < n; i++)
+//             cout << arr[i] << "";
+//         cout << "]";
+//     }
+// };
+// #endif 
+
+
+// 4-15 move constructor
 #ifndef VEC_F_H_INCLUDED
 #define VEC_F_H_INCLUDED
 #include <iostream>
@@ -54,6 +94,11 @@ public:
     VecF(const VecF& fv): n (fv.n) {
         arr = new float[n];
         memcpy(arr, fv.arr, sizeof(float) * n);
+    }
+
+    VecF(VecF&& fv): n (fv.n), arr (fv.arr) {
+        fv.arr = nullptr;
+        fv.n = 0;
     }
 
     VecF add(const VecF& fv) const {
