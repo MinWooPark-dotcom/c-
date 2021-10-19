44ef0a5a7df94684742bea4457ad1f1fd36cb06c
Minwoo Park <positive1attraction@gmail.com>
2021-10-19 22:18:36 +0900

    Function Overloading

---
+++ 윤성우의 열혈 C++ 프로그래밍/Chapter01/FunctionOverloading.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+void MyFunc(void)
+{
+    std::cout << "MyFunc(Void) called" << std::endl;
+}
+
+void MyFunc(char c)
+{
+    std::cout << "MyFunc(char c) called" << std::endl;
+}
+
+void MyFunc(int a, int b)
+{
+    std::cout << "MyFunc(int a, int b) called" << std::endl;
+}
+
+int main(void)
+{
+    MyFunc();
+    MyFunc('A');
+    MyFunc(12, 13);
+    return 0;
+}
+
+/*
+MyFunc(Void) called
+MyFunc(char c) called
+MyFunc(int a, int b) called
+*/
