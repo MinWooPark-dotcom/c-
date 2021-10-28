aee0833fc075b03c75f78d8aeeadf0ee3cede0d4
Minwoo Park <positive1attraction@gmail.com>
2021-10-28 15:15:49 +0900

    Use constructor instead of creating an initialization function

---
+++ 윤성우의 열혈 C++ 프로그래밍/Chapter04/FruitSaleSim2.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+class FruitSeller
+{
+    private:
+        int APPLE_PRICE;
+        int numOfApples;
+        int myMoney;
+    public:
+        // Constructor
+        FruitSeller(int price, int num, int money)
+        {
+            APPLE_PRICE = price;
+            numOfApples = num;
+            myMoney = money;
+        }
+        int SaleApples(int money)
+        {
+            int num = money / APPLE_PRICE;
+            numOfApples -= num;
+            myMoney += money;
+            return num;
+        }
+        void ShowSalesResult() const 
+        {
+            cout << "The remaining apples: " << numOfApples << endl;
+            cout << "Revenue from sales: " << myMoney << endl << endl;
+        }
+};
+
+class FruitBuyer
+{
+    private:
+        int myMoney;
+        int numOfApples;
+    public:
+        // Constructor
+        FruitBuyer(int money)
+        {
+            myMoney = money;
+            numOfApples = 0;
+        }
+        void BuyApples(FruitSeller &seller, int money)
+        {
+            numOfApples += seller.SaleApples(money);
+            myMoney -= money;
+        }
+        void ShowBuyResult() const
+        {
+            cout << "Current money: " << myMoney << endl;
+            cout << "Number of apples " << numOfApples << endl
+                 << endl;
+        }
+};
+
+int main(void)
+{
+    FruitSeller seller(1000, 20, 0);
+    FruitBuyer buyer(5000);
+    buyer.BuyApples(seller, 2000);
+
+    cout << "The status of fruit seller" << endl;
+    seller.ShowSalesResult();
+    cout << "The status of fruit buyer" << endl;
+    buyer.ShowBuyResult();
+    return 0;
+}
+
+/*
+The status of fruit seller
+The remaining apples: 18
+Revenue from sales: 2000
+
+The status of fruit buyer
+Current money: 3000
+Number of apples 2
+*/
